80e8a1b79e65c1a976fb02f4501b6827154de46d
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-04 16:37:00 -0600

    User story 6 complete for GolfCourse and Team

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -5,11 +5,23 @@ RSpec.describe "teams index page" do
     team_1 = Team.create(roster: 25, coach: true, league: "Avalanche")
     team_2 = Team.create(roster: 23, coach: false, league: "Blues")
     team_3 = Team.create(roster: 36, coach: true, league: "Reds")
-
+    
     visit "/teams"
-
+    
     expect(page).to have_content(team_1.league)
     expect(page).to have_content(team_2.league)
     expect(page).to have_content(team_3.league)
+  end
+  
+  it 'lists Teams by most recently created' do 
+    team_1 = Team.create(roster: 25, coach: true, league: "Avalanche")
+    team_2 = Team.create(roster: 23, coach: false, league: "Blues")
+    team_3 = Team.create(roster: 36, coach: true, league: "Reds")
+
+    visit "/teams"
+
+    expect(team_3.league).to appear_before(team_2.league)
+    expect(team_3.league).to appear_before(team_1.league)
+    expect(team_1.league).to_not appear_before(team_2.league)
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,9 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end

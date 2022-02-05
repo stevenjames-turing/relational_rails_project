6ce291b75e52a4e9e3ea9ed57e8ea68785154849
Blake Dempsey <bdempsey864@gmail.com>
2022-02-05 18:28:40 -0500

    user story 15 complete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.all
+    @players = Player.all.injured
   end
 
   def show
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :team
   # validates :name, :injured, :number, presence: true 
+  def self.injured
+    Player.all.where(injured: true)
+  end
 end

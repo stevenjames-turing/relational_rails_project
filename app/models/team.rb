576de329cd4fcdce8035c2dfd2afeb83d1009b92
Blake Dempsey <bdempsey864@gmail.com>
2022-02-07 17:45:13 -0500

    refactor user stories 6/7

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,12 @@
 class Team < ApplicationRecord
   has_many :players
   # validates :roster, :coach, :league, presence: true 
+
+  def self.sort_by_created_at
+    Team.order(created_at: :desc)
+  end
+
+  def number_of_players
+    players.count
+  end
 end
